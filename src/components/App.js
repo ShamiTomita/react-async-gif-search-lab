ae3d431fff088d37f99f3fbcf5255498f6fc92fc
Shami <shamitomita@gmail.com>
2022-02-07 17:46:36 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 import NavBar from './NavBar'
-
-// the App component should render out the GifListContainer component 
+import GifListContainer from "/Users/shamitomita/Flatiron/code/react-async-gif-search-lab/src/containers/GifListContainer.js"
+// the App component should render out the GifListContainer component DONEdoneDONE!
 
 const App = () => {
   return (
     <div>
+      <div>
         < NavBar color='black' title="Giphy Search" />
+      </div>
+      <div>
+      <GifListContainer />
+      </div>
     </div>
   )
 }
